543ed487aa3347958a0a251a13291b75e1259408
Sofiia Yermolaieva <sofia-ne@ukr.net>
2017-07-25 19:01:32 +0300

    Made a weekly calendar with scrolling.
    Made ability to scroll to previous and next weeks.
    Added images.

--- src/components/WeeklyCalendar.js
+++ src/components/WeeklyCalendar.js
@@ -2,6 +2,8 @@
  * Created by Sofiia Yermolaieva on 25.07.2017.
  */
 import React, { Component } from 'react';
+import arrow_LEFT from "../arrow_LEFT.png";
+import arrow_right from "../arrow_right.png";
 
 const MONTH = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август",
     "Сентябрь", "Октябрь","Ноябрь", "Декабрь"];
@@ -10,8 +12,8 @@ const WEEK_DAYS = ["Воскресенье", "Понедельник", "Втор
 class WeeklyCalendar extends Component {
     constructor(){
         super();
-        this.state = {day: new Date(),
-                        month: new Date().getUTCMonth()};
+        this.state = {todaysDay: new Date(),
+                        currentDay: ""};
     }
 
     weekCalendar(dayToCount) {
@@ -19,49 +21,88 @@ class WeeklyCalendar extends Component {
         let year = day.getFullYear();
         let month = day.getMonth();
         // will be needed for the blocking of booking ability
-        let currentDay = day.getDate();
-        let currentWeekDay = day.getDay();
         let last_day = new Date(year, month + 1, 0);
 
         // day of the month - day of the week + 1
         //Monday
-        let firstWorkingWeekDay = this.getFirstWorkingDay(day, currentDay, currentWeekDay);
-        // first day + 4
-        //Friday
-        let lastWorkingWeekDay = this.getLastWorkingDay(day, firstWorkingWeekDay);
+        let firstWorkingWeekDay = this.getFirstWorkingDay(day);
 
         let week = [];
 
         for(let i = 0; i < 5; i++){
             let weekDay;
 
-            if(i == 0){
+            if(i === 0){
                 weekDay = firstWorkingWeekDay;
-            }else if(i == 4){
-                weekDay = lastWorkingWeekDay;
             }else {
-                weekDay = new Date();
-                weekDay.setDate(firstWorkingWeekDay.getDate()+i);
+                weekDay = new Date(firstWorkingWeekDay.getFullYear(), firstWorkingWeekDay.getMonth(),
+                    firstWorkingWeekDay.getDate() + i);
             }
 
             week[i] = weekDay;
         }
 
-        return week.toString();
+        return week;
     }
 
-    getFirstWorkingDay(day, currentDay, currentWeekDay){
-        return new Date(day.setDate(currentDay - currentWeekDay + 1));
+    getFirstWorkingDay(day){
+        let dayToCount = day;
+        let currentDay = dayToCount.getDate();
+        let currentWeekDay = dayToCount.getDay();
+        return new Date(dayToCount.setDate(currentDay - currentWeekDay + 1));
     }
 
-    getLastWorkingDay(day, firstWorkingWeekDay) {
-        return new Date(day.setDate(firstWorkingWeekDay.getDate() + 4))
+    getPreviousWeek(){
+        let curentWeekDay = this.getCurrentWeekDay();
+
+        curentWeekDay = new Date(curentWeekDay.getFullYear(), curentWeekDay.getMonth(),
+            curentWeekDay.getDate() - 7);
+        
+        this.setState({currentDay: curentWeekDay});
+    }
+
+    getNextWeek(){
+        let curentWeekDay = this.getCurrentWeekDay();
+
+        curentWeekDay = new Date(curentWeekDay.getFullYear(), curentWeekDay.getMonth(),
+            curentWeekDay.getDate() + 7);
+
+        this.setState({currentDay: curentWeekDay});
+    }
+
+    getCurrentWeekDay(){
+        let curentWeekDay;
+        if(this.state.currentDay === ""){
+            curentWeekDay = this.state.todaysDay;
+        }else {
+            curentWeekDay = this.state.currentDay;
+        }
+
+        return curentWeekDay;
     }
 
     render(){
-        
+        let weekDay;
+
+        if(this.state.currentDay === ""){
+            weekDay = this.state.todaysDay;
+        }else {
+            weekDay = this.state.currentDay;
+        }
+
         return (
-            <div>{this.weekCalendar(new Date())}</div>
+            <div>
+                <div id="month">{MONTH[weekDay.getMonth()]}</div>
+                <button onClick={this.getPreviousWeek.bind(this)}><img src={arrow_LEFT} alt="Prev"/></button>
+                <ul>
+                    {
+                        this.weekCalendar(weekDay).map(function(day, i){
+                            return <li key={i}>{WEEK_DAYS[i + 1]} {day.getDate().toString()}</li>
+                        })
+                    }
+                </ul>
+                <button onClick={this.getNextWeek.bind(this)}><img src={arrow_right} alt="Next"/> </button>
+            </div>
         );
     }
 }
