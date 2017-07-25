cab964adfe5bbd53f9e16b99c36f85fb889db1df
Sofiia Yermolaieva <sofia-ne@ukr.net>
2017-07-25 15:42:32 +0300

    Changed function for weekly calendar calculation.
    Made a universal function to calculate weeks.

--- src/components/WeeklyCalendar.js
+++ src/components/WeeklyCalendar.js
@@ -8,30 +8,60 @@ const MONTH = ["Январь", "Февраль", "Март", "Апрель", "М
 const WEEK_DAYS = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
 class WeeklyCalendar extends Component {
-    calendar() {
-        let day = new Date();
+    constructor(){
+        super();
+        this.state = {day: new Date(),
+                        month: new Date().getUTCMonth()};
+    }
+
+    weekCalendar(dayToCount) {
+        let day = dayToCount;
         let year = day.getFullYear();
-        let month = day.getUTCMonth();
+        let month = day.getMonth();
         // will be needed for the blocking of booking ability
         let currentDay = day.getDate();
         let currentWeekDay = day.getDay();
+        let last_day = new Date(year, month + 1, 0);
 
         // day of the month - day of the week + 1
         //Monday
-        let firstWorkingWeekDay = new Date(day.setDate(currentDay - currentWeekDay + 1));
+        let firstWorkingWeekDay = this.getFirstWorkingDay(day, currentDay, currentWeekDay);
         // first day + 4
         //Friday
-        let lastWorkingWeekDay = new Date(day.setDate(firstWorkingWeekDay.getDate() + 4));
+        let lastWorkingWeekDay = this.getLastWorkingDay(day, firstWorkingWeekDay);
 
-        let last_day = new Date(year, month + 1, 0);
+        let week = [];
 
+        for(let i = 0; i < 5; i++){
+            let weekDay;
 
-        return "first day" + firstWorkingWeekDay.toUTCString() + " last day" + lastWorkingWeekDay.toUTCString();
+            if(i == 0){
+                weekDay = firstWorkingWeekDay;
+            }else if(i == 4){
+                weekDay = lastWorkingWeekDay;
+            }else {
+                weekDay = new Date();
+                weekDay.setDate(firstWorkingWeekDay.getDate()+i);
+            }
+
+            week[i] = weekDay;
+        }
+
+        return week.toString();
+    }
+
+    getFirstWorkingDay(day, currentDay, currentWeekDay){
+        return new Date(day.setDate(currentDay - currentWeekDay + 1));
+    }
+
+    getLastWorkingDay(day, firstWorkingWeekDay) {
+        return new Date(day.setDate(firstWorkingWeekDay.getDate() + 4))
     }
 
     render(){
+        
         return (
-            <div>{this.calendar()}</div>
+            <div>{this.weekCalendar(new Date())}</div>
         );
     }
 }
