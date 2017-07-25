1ec7325795077697ecf6104abefbd1651ed74b11
Sofiia Yermolaieva <sofia-ne@ukr.net>
2017-07-25 10:33:09 +0300

    Created aproject with create-react-app
    Added few initial templates for the room booking project.

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Sofiia Yermolaieva on 25.07.2017.
+ */
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render(){
+        return (
+            <div><div>Rooms</div><div>Calendar</div></div>
+        );
+    }
+}
+
+export default Content;
